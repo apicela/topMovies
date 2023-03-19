a5159bc1aa08dfa0c690c9e3f29229188573ee78
jamilzin1 <jamilnetobr@gmail.com>
2023-03-19 00:53:17 -0300

    action on dropdown

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+/* 
+preciso add region= para filtrar por país, entretanto, escolhendo provedor, há bug.
+*/
+
+
 const API_KEY = "api_key=4afe9ae58f077a64af133da919425eca";
 const BASE_URL = "https://api.themoviedb.org/3/";
 const LANGUAGE = "&language=pt-BR";
-const API_POPULAR_MOVIES = BASE_URL + 'movie/popular?&' + API_KEY + LANGUAGE + "&page=1" + "&vote_count.gte=100";
-const API_POPULAR_SERIES = BASE_URL + 'tv/popular?&' + API_KEY + LANGUAGE + "&page=1" + "&vote_count.gte=100";
+const API_POPULAR_MOVIES = BASE_URL +  'movie/popular?&' + API_KEY + LANGUAGE + "&page=1" + "&vote_count.gte=100";
+const API_POPULAR_SERIES = BASE_URL  + 'tv/popular?&' + API_KEY + LANGUAGE + "&page=1" + "&vote_count.gte=100";
 const API_DISCOVER_MOVIES = BASE_URL + "discover/movie?"+ API_KEY+ LANGUAGE+"&sort_by=release_date.desc" + "&vote_count.gte=100"; 
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
-var type="";
 const searchBar = document.getElementById('form');
 const search = document.getElementById('pesquisa');
 const SEARCH_URL = BASE_URL + 'search/multi?' + API_KEY; 
@@ -14,22 +18,56 @@ const main = document.getElementById('main');
 const watchFilmes = document.getElementById('filmes');
 const watchSeries = document.getElementById('series');
 const discoverMovies = document.getElementById('releases');
-const genreSelector = document.getElementById('generSelector');
+const genreSelector = document.getElementById('genreSelector');
+const filter = document.getElementById('filter');
+var type="movie/";
 
 console.log(API_POPULAR_MOVIES);
+console.log(API_DISCOVER_MOVIES);
 console.log(GENRE_LIST);
+
 getMovies(API_POPULAR_MOVIES);
 
 getList(GENRE_LIST);
 
-var countryBox = document.getElementById("selectCountry")
-.onchange = change => {
+
+function getAPI(){
     var country = document.getElementById("selectCountry").value;
+    var orderBy = document.getElementById("orderBy").value;
+    var provider = document.getElementById("provider").value;
+    var genre = document.getElementById("genreSelector").value;
+    var type = typeWatch();
+    console.log(BASE_URL + type+ orderBy+ API_KEY + LANGUAGE + "&page=1" +  
+    "&vote_count.gte=100"+  "&with_watch_providers=" + provider
+    +"&watch_region=" + country);    
+    return BASE_URL + type+ orderBy+ API_KEY + LANGUAGE + "&page=1" +  
+     "&vote_count.gte=100"+ "&with_watch_providers=" + provider
+     +"&watch_region=" + country;      
+}
 
-    getMovies(API_POPULAR_MOVIES+ "&region="+ country);}
-       /* else{getMovies(API_POPULAR_SERIES+ "&region="+ country);} 
-}*/
+var countryBox = document.getElementById("selectCountry")
+.onchange = () => {
+    getMovies(getAPI());
+}
 
+var orderByBox = document.getElementById("orderBy")
+.onchange = () => {
+    getMovies(getAPI());
+ }
+
+ var orderByBox = document.getElementById("provider")
+ .onchange = () => {
+     getMovies(getAPI());
+  }
+
+  var orderByBox = document.getElementById("genreSelector")
+.onchange = () => {
+    getMovies(getAPI());
+ }
+
+function typeWatch(){
+    return type;
+}
 
 function getMovies(url){ 
     fetch(url).then(res => res.json()).then(data => 
@@ -44,8 +82,7 @@ function getList(url){
 
 function genreList(data){
     genreSelector.innerHTML = `
-     <option value="" selected>Gênero</option>
-     <option>Todos</option>  `
+     <option value="" selected>Todos</option>  `
     data.forEach(genre => {
         
         const{name} = genre;
@@ -102,20 +139,23 @@ searchBar.addEventListener('submit',(e) => {
 
 
 watchFilmes.addEventListener('click',(e)=> {
+    type = 'movie/';
+    filter.classList.remove("noFilter");
+
     watchFilmes.classList.add("dourado");
     discoverMovies.classList.remove("dourado");
     watchSeries.classList.remove("dourado");
 var country = document.getElementById("selectCountry").value;
-    getMovies(API_POPULAR_MOVIES + "&region="+ country);}
+    getMovies(BASE_URL +  'movie/popular?&' + API_KEY + LANGUAGE + "&page=1" + "&vote_count.gte=100" + "&region="+ country);}
 
 )
 
 watchSeries.addEventListener('click',(e)=> {
-
+      filter.classList.remove("noFilter");
+    type = 'tv/';
     watchSeries.classList.add("dourado");
     discoverMovies.classList.remove("dourado");
     watchFilmes.classList.remove("dourado");
-    
     var country = document.getElementById("selectCountry").value;
     getMovies(API_POPULAR_SERIES + "&region="+ country);
     console.log(API_POPULAR_SERIES + "&region="+ country)}
@@ -123,7 +163,7 @@ watchSeries.addEventListener('click',(e)=> {
     )
 
     discoverMovies.addEventListener('click',(e)=> {
-
+        filter.classList.add("noFilter");
         discoverMovies.classList.add("dourado");
         watchFilmes.classList.remove("dourado");
         watchSeries.classList.remove("dourado");
